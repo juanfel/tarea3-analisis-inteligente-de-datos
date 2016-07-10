d5ca8981084ff0940846ab4a0d011c175d4955de
juanfel <jf3dsaber@gmail.com>
2016-07-10 16:19:20 -0400

    Pregunta c completa.
    -Agrega la posibilidad de imprimir el texto junto con su transformacion
    -Comenta el codigo para bajar los csv
    -Agrega toda la pregunta c junto con el codigo del tagger.

--- pregunta 2.py
+++ pregunta 2.py
@@ -1,14 +1,14 @@
 import re, time
-from nltk.corpus import stopwords
-from nltk import WordNetLemmatizer, word_tokenize
+from nltk.corpus import stopwords, wordnet
+from nltk import WordNetLemmatizer, word_tokenize, pos_tag
 from nltk.stem.porter import PorterStemmer
 import urllib
 import pandas as pd
 #pregunta a)
-train_data_url = "http://www.inf.utfsm.cl/~jnancu/stanford-subset/polarity.train"
-test_data_url = "http://www.inf.utfsm.cl/~jnancu/stanford-subset/polarity.dev"
-train_data_f = urllib.urlretrieve(train_data_url, "train_data.csv")
-test_data_f = urllib.urlretrieve(test_data_url, "test_data.csv")
+# train_data_url = "http://www.inf.utfsm.cl/~jnancu/stanford-subset/polarity.train"
+# test_data_url = "http://www.inf.utfsm.cl/~jnancu/stanford-subset/polarity.dev"
+# train_data_f = urllib.urlretrieve(train_data_url, "train_data.csv")
+# test_data_f = urllib.urlretrieve(test_data_url, "test_data.csv")
 ftr = open("train_data.csv", "r")
 fts = open("test_data.csv", "r")
 rows = [line.split(" ",1) for line in ftr.readlines()]
@@ -32,15 +32,62 @@ def word_extractor(text):
     wordstemmizer = PorterStemmer()
     commonwords = stopwords.words('english')
     text = re.sub(r'([a-z])\1+', r'\1\1',text)#substitute multiple letter by two
+    print text
     words = ""
-    wordtokens = [ wordstemmizer.stem_word(word.lower()) \
+    wordtokens = [ wordstemmizer.stem(word.lower()) \
                    for word in word_tokenize(text.decode('utf-8', 'ignore')) ]
     for word in wordtokens:
         if word not in commonwords:
             words+=" "+word
     return words
-word_extractor("I love to eat cake")
-word_extractor("I love eating cake")
-word_extractor("I loved eating the cake")
-word_extractor("I do not love eating cake")
-word_extractor("I don't love eating cake")
+
+print word_extractor("I love to eat cake")
+print word_extractor("I love eating cake")
+print word_extractor("I loved eating the cake")
+print word_extractor("I do not love eating cake")
+print word_extractor("I don't love eating cake") #En resultados aparece n't
+print word_extractor("I like drinking soda")
+print word_extractor("I dislike to eat meat")
+print word_extractor("I absolutely hate to eat cake")
+print word_extractor("I never liked to use stemming")
+
+##Pregunta c
+def get_wordnet_pos(treebank_tag):
+    #Tags entregados por pos_tag no son compatibles con wordnet, hay que
+    #convertirlos.
+    if treebank_tag.startswith('J'):
+        return wordnet.ADJ
+    elif treebank_tag.startswith('V'):
+        return wordnet.VERB
+    elif treebank_tag.startswith('N'):
+        return wordnet.NOUN
+    elif treebank_tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return wordnet.NOUN #Es el default para wordnet tambien
+## Es necesario saber que a parte de la oracion corresponde cada palabra
+## por eso se usa el pos_tag
+def word_extractor2(text):
+    wordlemmatizer = WordNetLemmatizer()
+    commonwords = stopwords.words('english')
+    text = re.sub(r'([a-z])\1+', r'\1\1',text)#substitute multiple letter by two
+    print text
+    words = ""
+    tagged_words = pos_tag(word_tokenize(text.decode('utf-8', 'ignore')))
+    wordtokens = [ wordlemmatizer.lemmatize(word.lower(),pos = get_wordnet_pos(tag)) \
+                   for word,tag in tagged_words ]
+    for word in wordtokens:
+        if word not in commonwords:
+            words+=" "+word
+    return words
+
+
+print word_extractor2("I love to eat cake")
+print word_extractor2("I love eating cake")
+print word_extractor2("I loved eating the cake")
+print word_extractor2("I do not love eating cake")
+print word_extractor2("I don't love eating cake") #En resultados aparece n't
+print word_extractor2("I like drinking soda")
+print word_extractor2("I dislike to eat meat")
+print word_extractor2("I absolutely hate to eat cake")
+print word_extractor2("I never liked to use stemming")
